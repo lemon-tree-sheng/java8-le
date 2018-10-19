928307d120c60be5e6c4235df296ec47ae057c13
sheng <sheng_704@126.com>
2018-10-19 17:07:02 +0800

    init commit

---
+++ src/main/java/org/sheng/java8le/Java8LeApplication.java
@@ -0,0 +1,12 @@
+package org.sheng.java8le;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8LeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8LeApplication.class, args);
+    }
+}
